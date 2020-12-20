021b4029069b924e1882cda13b08820365c0d4c3
paullee714 <ba93love@gmail.com>
2020-12-20 21:06:15 +0900

    Validation -> class Validation

--- unber-eats-backend/src/restaurants/dto/create-restaurant.dto.ts
+++ unber-eats-backend/src/restaurants/dto/create-restaurant.dto.ts
@@ -1,17 +1,25 @@
 import { ArgsType, Field, InputType } from "@nestjs/graphql";
+import { IsBoolean, IsString, Length } from "class-validator";
+
 
 @ArgsType() //ArgsType은 각 Field를 각 Argument로 분리 ...
 export class CreateRestaurantDto{
     
     @Field(type => String)
+    @IsString() //class validator
+    @Length(2,15)
     name:string
     
     @Field(type => Boolean)
+    @IsBoolean() //class validator 
     isVegan:boolean
     
     @Field(type => String)
+    @IsString() //class validator
+
     address:string
     
     @Field(type => String)
+    @IsString() //class validator
     ownerName:string
 }
