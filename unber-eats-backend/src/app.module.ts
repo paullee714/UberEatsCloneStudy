b70f5a79980c0f213e8011d71b18fc50c7fc43f3
paullee714 <ba93love@gmail.com>
2020-12-21 01:42:05 +0900

    TypeORM Setup

--- unber-eats-backend/src/app.module.ts
+++ unber-eats-backend/src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { join } from 'path';
 import { RestaurantsModule } from './restaurants/restaurants.module';
 
@@ -9,7 +10,17 @@ import { RestaurantsModule } from './restaurants/restaurants.module';
       // autoSchemaFile: join(process.cwd(), 'src/schema.gql'), // 파일을 직접 가지고 있어야 함 
       autoSchemaFile: true, // 파일을 직접 가지고 있지 않아도 됨 -> 온메모리
     }),
-    RestaurantsModule
+    RestaurantsModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres', 
+      host: 'localhost',
+      port: 5432,
+      username: 'wool',
+      password: 'qwerqwer123',
+      database: 'uber-eats',
+      synchronize: true,
+      logging: true,
+    })
   ],
   controllers: [],
   providers: [],
