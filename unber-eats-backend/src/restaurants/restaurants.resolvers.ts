8fc041dd5fe70c53d30373bb2dc92a1d1d1b273e
paullee714 <ba93love@gmail.com>
2020-12-20 20:43:51 +0900

    nestjs, graphQL Arguments

--- unber-eats-backend/src/restaurants/restaurants.resolvers.ts
+++ unber-eats-backend/src/restaurants/restaurants.resolvers.ts
@@ -1,13 +1,11 @@
-import { Query, Resolver } from "@nestjs/graphql";
+import { Args, Query, Resolver } from "@nestjs/graphql";
 import { Restaurant } from "./entities/restaurant.entity";
-
-
-@Resolver()
+@Resolver(of => Restaurant)
 export class RestaurantResolver{
     
-    @Query(returns => Restaurant)
-    myRestaurant(){
-        return true
+    @Query(returns => [Restaurant])
+    myRestaurant(@Args('veganOnly') VeganOnly: boolean):Restaurant[]{
+        return [];
     }
 
 }
