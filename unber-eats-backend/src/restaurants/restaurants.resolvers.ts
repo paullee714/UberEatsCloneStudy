a89c9dc36da5b956c19f075fc5b4edc6624ae095
paullee714 <ba93love@gmail.com>
2020-12-24 00:35:19 +0900

    Injecting The Repository - TypeORM / nest.js

--- unber-eats-backend/src/restaurants/restaurants.resolvers.ts
+++ unber-eats-backend/src/restaurants/restaurants.resolvers.ts
@@ -1,12 +1,15 @@
 import { Args, Mutation, Query, Resolver } from "@nestjs/graphql";
 import { CreateRestaurantDto } from "./dto/create-restaurant.dto";
 import { Restaurant } from "./entities/restaurant.entity";
+import { RestaurantService } from "./restaurants.service";
 @Resolver(of => Restaurant)
 export class RestaurantResolver{
-    
+
+    constructor(private readonly restaurantService: RestaurantService){}
+
     @Query(returns => [Restaurant])
-    myRestaurant(@Args('veganOnly') VeganOnly: boolean):Restaurant[]{
-        return [];
+    myRestaurant():Promise<Restaurant[]>{
+        return this.restaurantService.getAll();
     }
 
     @Mutation(returns => Boolean)
