603b8c1dee64a23f30244eb2e40cb55e577a6dff
paullee714 <ba93love@gmail.com>
2020-12-25 22:40:57 +0900

    user Create

--- unber-eats-backend/src/users/user.service.ts
+++ unber-eats-backend/src/users/user.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
+import { CreateAccountInput } from "./dtos/create-account.dto";
 import { User } from "./entities/user.entity";
 
 
@@ -9,4 +10,20 @@ export class UsersSerivce{
     constructor(
         @InjectRepository(User) private readonly users: Repository<User>
     ){}
+
+    async createAccount({email,password,role}:CreateAccountInput): Promise<string | undefined>{
+        // check User
+        // if doesNotExist => Create user & hash the password => return ok
+        try{
+            const exists = await this.users.findOne({email})
+            if(exists){
+                // User Exist -> return error 
+                return "There is a user with that email already";
+            }
+            await this.users.save(this.users.create({email,password,role}))
+        }catch(e){
+            console.log(e)
+            return "Couldn't create account"
+        }
+    }
 }
--- unber-eats-backend/src/users/users.resolver.ts
+++ unber-eats-backend/src/users/users.resolver.ts
@@ -1,4 +1,5 @@
-import { Query, Resolver } from "@nestjs/graphql";
+import { Args, Mutation, Query, Resolver } from "@nestjs/graphql";
+import { CreateAccountInput, CreateAccountOutPut } from "./dtos/create-account.dto";
 import { User } from "./entities/user.entity";
 import { UsersSerivce } from "./user.service";
 
@@ -14,6 +15,28 @@ export class UsersResolver{
     @Query(returns => Boolean)
     hi(){
         return true;
+    }
+
+    @Mutation(returns => CreateAccountOutPut)
+    async createAccount(@Args("input") createAccountInput: CreateAccountInput): Promise<CreateAccountOutPut>{
+        try{
+            const error = await this.userService.createAccount(createAccountInput);
+            if(error){
+                return{
+                    ok: false,
+                    error,
+                };
+            }
+            return{
+                ok:true,
+            }
+        }catch(error){
+            return{
+               error,
+               ok:false
+            }
+
+        }
     }
 
 }
