84f8853cf0993b2c9403959f9beecd6c2c9278ca
paullee714 <ba93love@gmail.com>
2021-01-01 14:34:38 +0900

    create Verification Insert Code

--- unber-eats-backend/src/users/entities/verification.entity.ts
+++ unber-eats-backend/src/users/entities/verification.entity.ts
@@ -1,6 +1,7 @@
+import { v4 as uuidv4 } from 'uuid';
 import { Field, InputType, ObjectType } from "@nestjs/graphql";
 import { CoreEntity } from "src/common/entities/core.entity";
-import { Column, Entity, JoinColumn, OneToOne } from "typeorm";
+import { BeforeInsert, Column, Entity, JoinColumn, OneToOne } from "typeorm";
 import { User } from "./user.entity";
 
 
@@ -18,6 +19,12 @@ export class Verification extends CoreEntity{
     @OneToOne(type => User)
     @JoinColumn()
     user: User;
+
+
+    @BeforeInsert()
+    createCode():void{
+        this.code = uuidv4();
+    }
 
 
 
--- unber-eats-backend/src/users/user.service.ts
+++ unber-eats-backend/src/users/user.service.ts
@@ -8,12 +8,14 @@ import * as jwt from "jsonwebtoken"
 import { ConfigService } from "@nestjs/config";
 import { JwtService } from "src/jwt/jwt.service";
 import { EditProfileInput } from "./dtos/edit-profile.dto";
+import { Verification } from "./entities/verification.entity";
 
 
 @Injectable()
 export class UsersSerivce{
     constructor(
         @InjectRepository(User) private readonly users: Repository<User>,
+        @InjectRepository(Verification) private readonly verifications: Repository<Verification>, //verification
         private readonly jwtService: JwtService,
     ){}
 
@@ -26,7 +28,12 @@ export class UsersSerivce{
                 // User Exist -> return error 
                 return {ok:false,error:"There is a user with that email already"};
             }
-            await this.users.save(this.users.create({email,password,role}))
+            const user = await this.users.save(this.users.create({email,password,role}))
+            //start verification
+            await this.verifications.save(this.verifications.create({
+                user
+            }))
+
             return {ok:true}
         }catch(e){
             console.log(e)
@@ -73,6 +80,9 @@ export class UsersSerivce{
         const user = await this.users.findOne(userId);
         if(email){
             user.email = email
+            user.verified = false;
+            await this.verifications.save(this.verifications.create({user})
+            )
         }
         if(password){
             user.password = password
